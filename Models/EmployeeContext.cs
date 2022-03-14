acdbe787723903d0c648e96843d44be84a25ee5a
NiklasDahlgrens <96531368+NiklasDahlgrens@users.noreply.github.com>
2022-03-14 16:38:32 +0100

    Add files via upload

---
+++ Models/EmployeeContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMVC.Models
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+        public EmployeeContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Models/ModelView.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMVC.Models
+{
+    public class ModelView
+    {
+        //Det här är Attributen som ni lägger till själv (Exempelvis Arrangör Attributen)
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SecNumber { get; set; }
+        public string TelephoneNr { get; set; }
+        public string Role { get; set; }
+
+        //Det här är kontokoden som lägger till i eran ViewModel
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int AccessLevel { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
